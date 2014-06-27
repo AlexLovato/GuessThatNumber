a0059d95fee2c4ae1a6f3a981799308bc598f322
Alex Lovato <alex.diego.lovato@gmail.com>
2014-06-27 08:43:32 -0600

    w/ comments
    updated comments

--- Guess that Number/Guess that Number/Program.cs
+++ Guess that Number/Guess that Number/Program.cs
@@ -9,30 +9,36 @@ namespace Guess_that_Number
     class Program
     {
         static void Main(string[] args)
-        {
+        {//greet them
            Console.WriteLine("Let's play a game.");
+            //call game function
            GameFunction();
+            //ask if they wanna play again
            Console.WriteLine("would you like to play again?");
            string answer = Console.ReadLine();
+            //put their answer to lowercase for comparison
            string lower = answer.ToLower();
+            //test their answer
            if (lower == "yes" || lower == "y")
-           {
+           {//if they type yes call game function 
                GameFunction();
            }
+               //their answer isn't yes so tell them bye
            else Console.WriteLine("peace out, dude");
         }
         static void GameFunction()
         {
-            
+            //make the computer think of a random number
             Console.WriteLine("I am thinking of a number.");
-            
+            //generate random number between including 2 and 100
             Random randomNumberGenerator = new Random();
             int randomNumber = randomNumberGenerator.Next(1, 101);
-
+            //ask for a guess
             Console.WriteLine("Type in your guess. :");
             int intInput = 0;
+            //while their guess is incorrecct
             while (intInput != randomNumber)
-            {
+            { //ask for a guess and tell them whether it's greater or lower than their guess
                 string newInput = Console.ReadLine();
                 intInput = Convert.ToInt32(newInput);
                 Console.WriteLine("nope, not " + newInput);
@@ -46,7 +52,7 @@ namespace Guess_that_Number
                 }
 
             }
-
+            //tell them they win if intInput == randomNumber (else)
             Console.WriteLine(intInput + "was correct, you win!");
             Console.ReadKey();
         }
